0c1c796aedda6e26c9e8b5e9dc56c18bc6139162
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2024-11-08 12:55:36 +0900

    namespace

--- Assets/AdMobApi/Scripts/DodgeBanner.cs
+++ Assets/AdMobApi/Scripts/DodgeBanner.cs
@@ -1,91 +1,93 @@
 ﻿using UnityEngine;
-using GoogleMobileAds.Utility;
 using System.Collections;
 
-/// <summary>
-/// アンカーバナー回避のためにサイズを調整する
-///		制御対象のオブジェクトにアタッチする
-///		オブジェクトは、アンカー(0, 0)-(1, 1)で配置し、バナーの配置に応じてピボットのYを0または1にする
-///		つまり、ここではサイズを制御するのみで、避ける方向は制御しない
-/// </summary>
-public class DodgeBanner : MonoBehaviour {
+namespace GoogleMobileAds.Utility {
 
-	/// <summary>対象のセット</summary>
-	[SerializeField] private string adSet = default;
+    /// <summary>
+    /// アンカーバナー回避のためにサイズを調整する
+    ///		制御対象のオブジェクトにアタッチする
+    ///		オブジェクトは、アンカー(0, 0)-(1, 1)で配置し、バナーの配置に応じてピボットのYを0または1にする
+    ///		つまり、ここではサイズを制御するのみで、避ける方向は制御しない
+    /// </summary>
+    public class DodgeBanner : MonoBehaviour {
 
-	/// <summary>対象のユニット番号(生成順) (通常は0)</summary>
-	[SerializeField] private int unit = default;
+        /// <summary>対象のセット</summary>
+        [SerializeField] private string adSet = default;
 
-	/// <summary>避け始める変位量 (超えるまで避けず、越えた分だけ避ける)</summary>
-	[SerializeField] private float threshold = 0f;
+        /// <summary>対象のユニット番号(生成順) (通常は0)</summary>
+        [SerializeField] private int unit = default;
+
+        /// <summary>避け始める変位量 (超えるまで避けず、越えた分だけ避ける)</summary>
+        [SerializeField] private float threshold = 0f;
 
 #if (UNITY_ANDROID || UNITY_IPHONE) && ALLOW_ADS
 
-	/// <summary>対象の外部設定</summary>
-	public void SetTarget (string set, int unit) {
-		adSet = set;
-		this.unit = unit;
-		targetAds = null;
-		updateSize ();
-	}
+        /// <summary>対象の外部設定</summary>
+        public void SetTarget (string set, int unit) {
+            adSet = set;
+            this.unit = unit;
+            targetAds = null;
+            updateSize ();
+        }
 
-	/// <summary>初期化完了</summary>
-	private bool inited = false;
+        /// <summary>初期化完了</summary>
+        private bool inited = false;
 
-	/// <summary>対象のバナー広告</summary>
-	private AdMobApi targetAds {
-		get => _targetAds ??= AdMobApi.GetSceneAds (adSet, unit);
-		set => _targetAds = value;
-	}
-	private AdMobApi _targetAds;
+        /// <summary>対象のバナー広告</summary>
+        private AdMobApi targetAds {
+            get => _targetAds ??= AdMobApi.GetSceneAds (adSet, unit);
+            set => _targetAds = value;
+        }
+        private AdMobApi _targetAds;
 
-	/// <summary>バナー状態</summary>
-	private bool isActive => AdMobApi.Allow && AdMobApi.GetActive (adSet);
+        /// <summary>バナー状態</summary>
+        private bool isActive => AdMobApi.Allow && AdMobApi.GetActive (adSet);
 
-	/// <summary>初期サイズ</summary>
-	private Vector2 initialSize;
+        /// <summary>初期サイズ</summary>
+        private Vector2 initialSize;
 
-	/// <summary>サイズ調整対象 (初期sizeDelta (0, 0))</summary>
-	private RectTransform rect;
+        /// <summary>サイズ調整対象 (初期sizeDelta (0, 0))</summary>
+        private RectTransform rect;
 
-	/// <summary>初期化</summary>
-	private IEnumerator Start () {
-		rect = transform as RectTransform;
-		initialSize = rect.sizeDelta;
-		yield return new WaitUntil (() => targetAds != null); // バナーの初期化を待つ
-		if (targetAds.Type != AdType.Banner) {
-			throw new System.ArgumentOutOfRangeException ($"'{adSet}' is not Banner.");
-		}
-		inited = true;
-		if (isActive) {
-			// 先んじてバナーが表示されていた場合はここで初期化が必要
-			updateSize ();
-			// 空読み
-			_ = targetAds?.Dirty;
-		}
-		//Debug.Log ($"DodgeBanner.Start {name} {(inited ? "inited" : "init error")} {targetAds.Scene}:{targetAds.Unit} {(isActive ? "active" : "inactive")}");
-	}
+        /// <summary>初期化</summary>
+        private IEnumerator Start () {
+            rect = transform as RectTransform;
+            initialSize = rect.sizeDelta;
+            yield return new WaitUntil (() => targetAds != null); // バナーの初期化を待つ
+            if (targetAds.Type != AdType.Banner) {
+                throw new System.ArgumentOutOfRangeException ($"'{adSet}' is not Banner.");
+            }
+            inited = true;
+            if (isActive) {
+                // 先んじてバナーが表示されていた場合はここで初期化が必要
+                updateSize ();
+                // 空読み
+                _ = targetAds?.Dirty;
+            }
+            //Debug.Log ($"DodgeBanner.Start {name} {(inited ? "inited" : "init error")} {targetAds.Scene}:{targetAds.Unit} {(isActive ? "active" : "inactive")}");
+        }
 
-	/// <summary>サイズ調整</summary>
-	private void updateSize () {
-		var size = initialSize;
-		if (isActive && targetAds != null) { // 回線接続がないとサイズが取得できないので、都度取得することが望ましい
-			var height = targetAds.BannerPixelSize.y / rect.lossyScale.y - threshold;
-			//Debug.Log ($"DodgeBanner {name} {height} {targetAds.BannerPixelSize.y} / {rect.lossyScale.y} - {threshold}");
-			if (height > 0f) {
-				size.y -= height;
-			}
-		}
-		rect.sizeDelta = size;
-	}
+        /// <summary>サイズ調整</summary>
+        private void updateSize () {
+            var size = initialSize;
+            if (isActive && targetAds != null) { // 回線接続がないとサイズが取得できないので、都度取得することが望ましい
+                var height = targetAds.BannerPixelSize.y / rect.lossyScale.y - threshold;
+                //Debug.Log ($"DodgeBanner {name} {height} {targetAds.BannerPixelSize.y} / {rect.lossyScale.y} - {threshold}");
+                if (height > 0f) {
+                    size.y -= height;
+                }
+            }
+            rect.sizeDelta = size;
+        }
 
-	/// <summary>駆動</summary>
-	private void Update () {
-		if (inited && targetAds?.Dirty == true) {
-			// 遅延して状態が変化した場合に応じる
-			updateSize ();
-		}
-	}
+        /// <summary>駆動</summary>
+        private void Update () {
+            if (inited && targetAds?.Dirty == true) {
+                // 遅延して状態が変化した場合に応じる
+                updateSize ();
+            }
+        }
 
 #endif
+    }
 }
